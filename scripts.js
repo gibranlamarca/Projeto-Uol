27d080eb209fa15ed20f7dcb5afda2f0051dd459
gibranlamarca <gibran.lamarca@engenharia.ufjf.br>
2020-09-17 14:16:59 -0300

    Envio de user e de chat funcionando

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
-var user=0;
+var user={name: 0};
+var pegaInput = 0;
+var tamanhoArray = 0;
+var destino = "Todos";
+var tipo = "message";
+
 function pegaUser(){
-    user = prompt("Digite o seu nick:");
+    user.name = prompt("Digite o seu nick:");
+    enviaUser();
 }
 pegaUser();
+function enviaUser(){
+    var enviar = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/uol/participants',user);
+}
+function mantemOnline(){
+    var enviar = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/uol/status',user);
+}
+setInterval(mantemOnline,4500);
+
+
+//parte do menu
 function abrirMenu(){
     var menu = document.querySelector("nav");
     menu.style.right="0";
@@ -18,11 +34,13 @@ function fecharMenu(){
     escurecer.style.display="none";
 }
 
+
+//parte dos chats
 function buscarDados(){
     var requisicao = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v1/uol/messages');
     requisicao.then(criaChat);
 }
-buscarDados();
+//setInterval(buscarDados,5000);
 
 function criaChat(chats){
     var dados = chats.data;
@@ -33,68 +51,69 @@ function criaChat(chats){
         document.querySelector("section").appendChild(caixaStatus);
         caixaStatus.classList.add('entra-sai');
 
-        var hora = document.createElement("div");
-        caixaStatus.appendChild(hora);
-        hora.classList.add('hora');
-        hora.innerText="("+dados[i].time+")";
-
-        var nome = document.createElement("div");
-        caixaStatus.appendChild(nome);
-        nome.classList.add('nome');
-        nome.innerText=dados[i].from;
-
-        var texto = document.createElement("div");
-        caixaStatus.appendChild(texto);
-        texto.classList.add('texto');
-        texto.innerText=dados[i].text;
+        var mensagem = document.createElement("p");
+        caixaStatus.appendChild(mensagem);
+        mensagem.innerHTML="<span style='color:#AAAAAA'>"+"("+dados[i].time+") "+"</span>"+"<strong>"+dados[i].from+"</strong>"+" "+dados[i].text;
         }
         else{
-            if(dados[i].type=='private_message' && dados[i].to==user){//tenho que adicionar um && checkando se o user do destinatário for o mesmo do que está usando o chat
+            if(dados[i].type=='private_message' && dados[i].to==user){
+
                 var caixaStatus = document.createElement("div");
                 document.querySelector("section").appendChild(caixaStatus);
                 caixaStatus.classList.add('reservada');
 
-                var hora = document.createElement("div");
+                var hora = document.createElement("time");
                 caixaStatus.appendChild(hora);
                 hora.classList.add('hora');
                 hora.innerText="("+dados[i].time+")";
 
-                var nome = document.createElement("div");
+                var nome = document.createElement("h1");
                 caixaStatus.appendChild(nome);
                 nome.classList.add('nome');
                 nome.innerText=dados[i].from;
 
-                var texto = document.createElement("div");
+                var texto = document.createElement("p");
                 caixaStatus.appendChild(texto);
                 texto.classList.add('texto');
                 texto.innerText=dados[i].text;
+                
             }
             else{
                 if(dados[i].type=='private_message')
                 {
-                    
+
                 }
                 else{
                 var caixaStatus = document.createElement("div");
                 document.querySelector("section").appendChild(caixaStatus);
                 caixaStatus.classList.add('mensagem');
 
-                var hora = document.createElement("div");
-                caixaStatus.appendChild(hora);
-                hora.classList.add('hora');
-                hora.innerText="("+dados[i].time+")";
-
-                var nome = document.createElement("div");
-                caixaStatus.appendChild(nome);
-                nome.classList.add('nome');
-                nome.innerText=dados[i].from;
-
-                var texto = document.createElement("div");
-                caixaStatus.appendChild(texto);
-                texto.classList.add('texto');
-                texto.innerText=dados[i].text;
+                var mensagem = document.createElement("p");
+                caixaStatus.appendChild(mensagem);
+                mensagem.innerHTML="<span style='color:#AAAAAA'>"+"("+dados[i].time+") "+"</span>"+"<strong>"+dados[i].from+"</strong>"+" "+"para "+"<strong>"+dados[i].to+"</strong>"+":"+" "+dados[i].text;
                 }
             }
         }
     }
+    caixaStatus.scrollIntoView();
+}
+function renderizaChat(){
+                pegaInput = document.querySelector("input").value;
+
+                var caixaStatus = document.createElement("div");
+                document.querySelector("section").appendChild(caixaStatus);
+                caixaStatus.classList.add('mensagem');
+
+                var mensagem = document.createElement("p");
+                caixaStatus.appendChild(mensagem);
+                mensagem.innerHTML="<strong>"+user.name+"</strong>"+" "+"para "+"<strong>"+destino+"</strong>"+":"+" "+pegaInput; //quando clicar em um usuário para mandar a mensagem para ele, vai alterar o valor da variável destino para o nick do usuário destinado
+
+                caixaStatus.scrollIntoView();
+                enviaChat();
+}
+function enviaChat(){
+    var dados = {from: user.name, to: destino, text: pegaInput, type: tipo};
+    
+    var requisicao = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/uol/messages', dados);
+
 }
